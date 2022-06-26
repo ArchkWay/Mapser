6b7fae98810e5b505d6648e7934c46d2cd93bce2
Viktor Ovchinnikov <v.ovchinnikov@1er.app>
2022-06-26 20:08:59 +0300

    initial commit

---
+++ app/src/main/java/com/example/mapser/MapsActivity.kt
@@ -0,0 +1,99 @@
+package com.example.mapser
+
+import android.os.Bundle
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mapser.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private var locationA: LatLng? = null
+    private var countMarkers = 0
+    private val destination = 0.002
+    private val START_LNG = -180.0
+    private val END_LNG = 180.0
+    private val START_LAT = -85.0
+    private val ZERO = 0.0
+    private val END_LAT = 85.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        val btnClear = findViewById<Button>(R.id.btnClear)
+        val btnA = findViewById<Button>(R.id.btnA)
+        val btnB = findViewById<Button>(R.id.btnB)
+        btnClear.setOnClickListener {
+            mMap.clear()
+            btnA.visibility = VISIBLE
+            btnB.visibility = VISIBLE
+            countMarkers = 0
+        }
+        btnA.setOnClickListener {
+            createMarker(mMap, getString(R.string.a), it)
+        }
+        btnB.setOnClickListener {
+            createMarker(mMap, getString(R.string.b),it)
+        }
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val rostov = LatLng(47.2313, 39.7233)//default location Rostov
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(rostov))
+    }
+
+    private fun polyline(locationB: LatLng) {
+        locationA?.let { locationA ->
+            createPolyline(locationA, locationB)
+            createPolyline(LatLng(locationA.latitude - destination, locationA.longitude - destination),
+                LatLng(locationB.latitude - destination, locationB.longitude - destination))
+            createPolyline(LatLng(locationA.latitude + destination, locationA.longitude + destination),
+                LatLng(locationB.latitude + destination, locationB.longitude + destination))
+        }
+    }
+
+    private fun createPolyline(a:LatLng, b: LatLng) {
+        val polylineOptions = PolylineOptions()
+            .add(LatLng(a.latitude, a.longitude))
+            .add(LatLng(b.latitude, b.longitude))
+            .add(LatLng(ZERO + b.latitude + b.latitude, END_LNG))
+            .add(LatLng(a.latitude, a.longitude))
+        mMap.addPolyline(polylineOptions)
+    }
+
+
+    private fun createMarker(mMap: GoogleMap, title: String, button: View) {
+        if(countMarkers < 2) {
+            countMarkers++
+            mMap.setOnMapClickListener {
+                println("__ lat ${it.latitude}")
+                println("__ lng ${it.longitude}")
+                when(countMarkers) {
+                    1 -> locationA = it
+                    2 -> polyline(it)
+                }
+                mMap.addMarker(MarkerOptions().position(it).title(title))
+                mMap.setOnMapClickListener {  }
+                button.visibility = GONE
+            }
+        }
+    }
+}
